fd8be384279e5ad87627a1b42fafab9f2d1b39bd
tonistorres <sisvenda2011@gmail.com>
2022-08-13 12:48:37 -0300

    feat: implementing function test_relatorio_produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,26 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    # importando a class e fazendo um mock dos seus valores iniciais
+    # passando valores ficticios para dos atributos que são carregados
+    # e necessitam de valores no momento que é instanciada essa classe
+    product = Product(
+        100,
+        "Agua Sanitaria 1000ml",
+        "Almoxarifado Central",
+        "01/01/2022",
+        "30/01/2025",
+        "Serie1234Xlblau",
+        "ali"
+    )
+
+    str_product = product.__repr__()
+
+    assert str_product == (
+        "O produto Agua Sanitaria 1000ml"
+        " fabricado em 01/01/2022"
+        " por Almoxarifado Central com validade"
+        " até 30/01/2025"
+        " precisa ser armazenado ali."
+    )
